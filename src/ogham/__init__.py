94f3f5c0859da8234bb817069897cca383f9dfd3
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2021-11-01 13:33:54 +0200

    Add letters to ogham function

--- src/ogham/__init__.py
+++ src/ogham/__init__.py
@@ -1,4 +1,4 @@
-from .mappings import get_ogham_mapping
+from . import mappings
 
 
 def transform(content: str, mapping: 'dict[str, str]') -> str:
@@ -9,9 +9,26 @@ def transform(content: str, mapping: 'dict[str, str]') -> str:
 
     return result
 
+def replace(content: str, mapping: 'dict[str, str]') -> str:
+    result = content
+
+    for dual_letter, ogham in mapping.items():
+        result = result.replace(dual_letter, ogham)
+
+    return result
 
 def ogham_to_letters(content: str) -> str:
-    mapping = get_ogham_mapping()
+    mapping = mappings.get_ogham_mapping()
     result = transform(content, mapping)
 
     return result
+
+def letters_to_ogham(content: str) -> str:
+    letter_mapping = mappings.get_letter_mapping()
+    dual_mapping = mappings.get_dual_letter_mapping()
+
+    dual_letter_result = replace(content, dual_mapping)
+    result = transform(dual_letter_result, letter_mapping)
+
+    # Append expected start/end characters.
+    return "᚛" + result + "᚜"
